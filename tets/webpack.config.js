2cc0c50618219b05cd6198d56c8b0681a561b756
grapeqna <101394346+grapeqna@users.noreply.github.com>
2024-05-21 08:46:25 +0300

    webpack

--- tets/webpack.config.js
+++ tets/webpack.config.js
@@ -4,28 +4,19 @@ const webpack = require('webpack');
 module.exports = (env, argv) => ({
   mode: argv.mode === 'production' ? 'production' : 'development',
 
-  // This is necessary because Figma's 'eval' works differently than normal eval
   devtool: argv.mode === 'production' ? false : 'inline-source-map',
   entry: {
-    code: './src/code.ts' // This is the entry point for our plugin code.
+    code: './src/code.ts' 
   },
   module: {
     rules: [
-      // Converts TypeScript code to JavaScript
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
-      // {
-      //   test: /\.css$/,
-      //   loader: [{ loader: 'style-loader' },
-      //   { loader: 'css-loader' }],
-      //   exclude: /node_modules/
-      // },
     ],
   },
-  // Webpack tries these extensions for you if you omit the extension like "import './file'"
   resolve: {
     extensions: ['.ts', '.js'],
   },
